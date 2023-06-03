1d3833f6eaf784ccc1427ff409d8523673946097
EC2 Default User <ec2-user@ip-172-31-37-83.ec2.internal>
2023-06-03 05:02:20 +0000

    first commit

---
+++ modules/globalvars/output.tf
@@ -0,0 +1,13 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "AparnaManoj"
+    "App"   = "Web"
+    "Project" = "AparnaAssignment"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "assignment1"
+}
